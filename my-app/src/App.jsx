65ebf375d961488ef531d77dab9f72bdc14ab4c2
sartaj <hajamsaj@gmail.com>
2025-03-15 14:24:35 +0530

    added custom hook using state management

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,14 +2,26 @@ import { useState } from 'react'
 
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0);
+// custom hook 
+// function only which starts with use and uses another hook inside it
+// custom hooks are used to share logic between components
+// custom hooks are not used to render anything
 
+function useCounter() {
+  const [count, setCount] = useState(0);
   function increaseCount() {
     setCount(count + 1);
   } 
+  return {
+    count : count,
+    increaseCount : increaseCount
+  }
+}
 
-
+function App() {
+  // using custom hook
+  const { count, increaseCount } = useCounter();
+  
     return (
       <div>
         <button  onClick={increaseCount}>Increase {count}</button>
